23a1f86ecee2929b425c288937487938b6d593db
Erin Payne <erinpayne@Erins-MBP-2.lab>
2019-04-16 17:32:37 -0500

    Untested first draft of main.c USART Handler

--- Lab6/src/main.c
+++ Lab6/src/main.c
@@ -12,6 +12,12 @@
 #include "stm32l4xx_hal.h"
 #include "stm32l475e_iot01.h"
 #include "string.h"
+#include "USART.h"
+#include "circularBuffer.h"
+
+static uint32_t USARTCount = 0;
+static commBuffer_t RX;
+static commBuffer_t TX;
 
 int main(void)
 {
@@ -22,53 +28,26 @@ int main(void)
 	/* Configure UART4 */
 	Configure_USART();
 
-	/* Configure the User LED */
-	BSP_LED_Init(LED2);
-	BSP_LED_Off(LED2);
-
-	uint32_t PrevTicks = HAL_GetTick();
-	uint32_t CurrentTicks = HAL_GetTick();
-
-	char* startText= "\n{\"Action\":\"Debug\",\"Info\":\"Testing UART4\"}\n";
-	SendCharArrayUSART4(startText,strlen(startText));
-	int strcnt = 0;
-	int timelapse = 0;
-	/* Infinite loop */
-	while(1){
-		CurrentTicks = HAL_GetTick();
-		if (haveString){
-				strcnt = strlen(receivedString);
-				timelapse = strcnt*500;
-				BSP_LED_On(LED2);
-				PrevTicks = HAL_GetTick();
-				SendCharArrayUSART4(receivedString,strcnt);
-				SendCharArrayUSART4("\n",strlen("\n"));
-				haveString = 0;
-		}
-		else{
-			if((CurrentTicks-PrevTicks) >= timelapse){
-				BSP_LED_Off(LED2);
-				strcnt = 0;
-			}
-		}
-	}
+	//Initializes Buffers
+	initBuffer(&RX, 0);
+	initBuffer(&TX, 1);
 }
 /**
   * @brief  System Clock Configuration
-  *         The system Clock is configured as follow :
-  *            System Clock source            = PLL (MSI)
-  *            SYSCLK(Hz)                     = 80000000
-  *            HCLK(Hz)                       = 80000000
-  *            AHB Prescaler                  = 1
-  *            APB1 Prescaler                 = 1
-  *            APB2 Prescaler                 = 1
-  *            MSI Frequency(Hz)              = 4000000
-  *            PLL_M                          = 1
-  *            PLL_N                          = 40
-  *            PLL_R                          = 2
-  *            PLL_P                          = 7
-  *            PLL_Q                          = 4
-  *            Flash Latency(WS)              = 4
+  * The system Clock is configured as follow :
+  * System Clock source            = PLL (MSI)
+  * SYSCLK(Hz)                     = 80000000
+  * HCLK(Hz)                       = 80000000
+  * AHB Prescaler                  = 1
+  * APB1 Prescaler                 = 1
+  * APB2 Prescaler                 = 1
+  * MSI Frequency(Hz)              = 4000000
+  * PLL_M                          = 1
+  * PLL_N                          = 40
+  * PLL_R                          = 2
+  * PLL_P                          = 7
+  * PLL_Q                          = 4
+  * Flash Latency(WS)              = 4
   * @param  None
   * @retval None
   */
@@ -112,22 +91,17 @@ static void SystemClock_Config(void)
  * where chars added. Tail index oldest string to be processed
  */
 void USARTx_IRQHandler(void) {
-	// check if the USART6 receive interrupt flag was set
+	// check if the USART6 receive interrupt flag is active
 	if(LL_USART_IsActiveFlag_RXNE(USARTx_INSTANCE)){
-
-		static uint8_t cnt = 0;
-		char t = LL_USART_ReceiveData8(USARTx_INSTANCE);
-		/* check if the received character is not the LF character (used to determine end of string)
-		 * or the if the maximum string length has been been reached
-		 */
-		if ( !((t == '\n')||(t == '\r')) && (cnt < MAX_STRLEN) ){
-			receivedString[cnt] = t;
-			cnt++;
-		}
-		else{ // otherwise reset the character counter and print the received string
-			receivedString[cnt] = 0x0;
-			cnt = 0;
-			haveString = 1;
-		}
+		putChar(&RX, LL_USART_ReceiveData8(USARTx_INSTANCE));
 	}
+
+	if(LL_USART_IsActiveFlag_TXE(USARTx_INSTANCE)){
+		if(haveMessage(&TX)){
+			LL_USART_TransmitData8(USARTx_INSTANCE, getChar(&TX));
+		}
+		else{
+			LL_USART_DisableIT_TXE(USARTx_INSTANCE);
+		}
+	}++USARTCount;
 }
